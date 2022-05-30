efba17c677a30101ac48ccab789a411691cec296
PeterStanger92020362 <65217016+PeterStanger92020362@users.noreply.github.com>
2022-05-30 21:09:18 +1200

    added createUser end point

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,9 +1,25 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
+import { CreateUserDto } from './dtos/create-user.dto';
+
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    ) {}
+
+    
+  async createUser(createUserDTO: CreateUserDto) {
+    const user = await this.prisma.user.create({
+      data: {
+        ...createUserDTO,
+      },
+    });
+
+    return user;
+  }
+
 
   async findById(id: string) {
     const existingUser = await this.prisma.user.findUnique({
