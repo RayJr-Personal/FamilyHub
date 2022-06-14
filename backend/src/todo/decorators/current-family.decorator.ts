f9d8646cc76032a43b9643347416053d89c437f7
MageBruh <raymondstevejr@yahoo.com>
2022-06-14 18:07:13 +1200

    Removed todo, being worked on in another branch

--- backend/src/todo/decorators/current-family.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const CurrentFamily = createParamDecorator(
-  (_data: never, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest();
-    return request.currentFamily;
-  },
-);
