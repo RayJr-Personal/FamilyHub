20007016a9ef9aa9f8e0c067b22409c509ee36a7
MageBruh <raymondstevejr@yahoo.com>
2022-08-08 21:25:00 +1200

    Very barebones layout built for Dashboard

--- frontend/familyhub/pages/dashboard.js
+++ frontend/familyhub/pages/dashboard.js
@@ -27,10 +27,23 @@ export default function Home() {
 
         <div className={`${styles2.feed} ${styles2.mainChild}`}>
           <h2>Feed</h2>
+          <div className={styles2.feedItem}>
+            <p>
+              <span className={styles2.feedItemPoster}>Example member</span>{" "}
+              posted:
+            </p>
+            <p className={styles2.feedItemTime}>1 hr 12 m ago</p>
+            <div className={styles2.feedItemBody}>
+              <p>insert picture here</p>
+              <p>Lorem ipsum...</p>
+            </div>
+          </div>
         </div>
 
         <div className={`${styles2.members} ${styles2.mainChild}`}>
           <h2>Family members</h2>
+          <p>Member 1</p>
+          <p>Member 2</p>
         </div>
       </main>
 
