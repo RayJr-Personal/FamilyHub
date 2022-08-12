f0ee30c1083c5e818410cedc319321a8d33c97ad
Adam Spice <adam@tella.co.nz>
2022-08-13 09:38:02 +1200

    feat: added user list to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,62 @@
-import { Fragment } from 'react'
-import Link from 'next/link'
-import { Popover, Transition } from '@headlessui/react'
-import clsx from 'clsx'
+import { Fragment } from "react";
+import Link from "next/link";
+import { Popover, Transition } from "@headlessui/react";
+import clsx from "clsx";
 
-import { Button } from '@/components/Button'
-import { Container } from '@/components/Container'
-import { Logo } from '@/components/Logo'
-import { NavLink } from '@/components/NavLink'
+import { Button } from "@/components/Button";
+import { Container } from "@/components/Container";
+import { Logo } from "@/components/Logo";
+import { NavLink } from "@/components/NavLink";
+import UserGroupIcon from "./icons/UserGroupIcon";
+
+import avatarImage1 from "@/images/avatars/avatar-1.png";
+import avatarImage2 from "@/images/avatars/avatar-2.png";
+import avatarImage3 from "@/images/avatars/avatar-3.png";
+import avatarImage4 from "@/images/avatars/avatar-4.png";
+import avatarImage5 from "@/images/avatars/avatar-5.png";
+
+import { UserList } from "./UserList";
+
+const users = [
+  {
+    id: "1",
+    name: "Sheryl Berge",
+    image: avatarImage1,
+  },
+  {
+    id: "2",
+    name: "Amy Hahn",
+    image: avatarImage4,
+  },
+
+  {
+    id: "3",
+    name: "Leland Kiehn",
+    image: avatarImage5,
+  },
+  {
+    id: "4",
+    name: "Erin Powlowski",
+    image: avatarImage2,
+  },
+  {
+    id: "5",
+    name: "Peter Renolds",
+    image: avatarImage3,
+  },
+  {
+    id: "6",
+    name: "Amy Hahn",
+    image: avatarImage4,
+  },
+];
 
 function MobileNavLink({ href, children }) {
   return (
     <Popover.Button as={Link} href={href} className="block w-full p-2">
       {children}
     </Popover.Button>
-  )
+  );
 }
 
 function MobileNavIcon({ open }) {
@@ -28,19 +71,19 @@ function MobileNavIcon({ open }) {
       <path
         d="M0 1H14M0 7H14M0 13H14"
         className={clsx(
-          'origin-center transition',
-          open && 'scale-90 opacity-0'
+          "origin-center transition",
+          open && "scale-90 opacity-0"
         )}
       />
       <path
         d="M2 2L12 12M12 2L2 12"
         className={clsx(
-          'origin-center transition',
-          !open && 'scale-90 opacity-0'
+          "origin-center transition",
+          !open && "scale-90 opacity-0"
         )}
       />
     </svg>
-  )
+  );
 }
 
 function MobileNavigation() {
@@ -86,7 +129,49 @@ function MobileNavigation() {
         </Transition.Child>
       </Transition.Root>
     </Popover>
-  )
+  );
+}
+
+function UserListPanel() {
+  return (
+    <Popover>
+      <Popover.Button
+        className="relative z-10 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none"
+        aria-label="Toggle Navigation"
+      >
+        {({ open }) => <UserGroupIcon open={open} />}
+      </Popover.Button>
+      <Transition.Root>
+        <Transition.Child
+          as={Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="duration-150 ease-in"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Popover.Overlay className="fixed inset-0 bg-slate-300/50" />
+        </Transition.Child>
+        <Transition.Child
+          as={Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Popover.Panel
+            as="div"
+            className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5"
+          >
+            <UserList users={users} />
+          </Popover.Panel>
+        </Transition.Child>
+      </Transition.Root>
+    </Popover>
+  );
 }
 
 export function Header() {
@@ -94,31 +179,29 @@ export function Header() {
     <header className="py-10">
       <Container>
         <nav className="relative z-50 flex justify-between">
-          <div className="flex items-center md:gap-x-12">
-            <Link href="#" aria-label="Home">
-              <Logo className="h-10 w-auto" />
-            </Link>
-            <div className="hidden md:flex md:gap-x-6">
-              <NavLink href="#features">Features</NavLink>
-              <NavLink href="#testimonials">Testimonials</NavLink>
-              <NavLink href="#pricing">Pricing</NavLink>
-            </div>
-          </div>
-          <div className="flex items-center gap-x-5 md:gap-x-8">
-            <div className="hidden md:block">
-              <NavLink href="/login">Sign in</NavLink>
-            </div>
-            <Button href="/register" color="blue">
-              <span>
-                Get started <span className="hidden lg:inline">today</span>
-              </span>
-            </Button>
+          <div className="flex w-full items-center justify-between md:gap-x-12">
             <div className="-mr-1 md:hidden">
               <MobileNavigation />
             </div>
+            <Link href="#" aria-label="Home">
+              <Logo className="h-10 w-auto" />
+            </Link>
+            <div className="flex justify-between items-center">
+              <div className="mr-2 ">
+                <UserListPanel />
+              </div>
+              <div className="w-20 hidden md:block items-center">
+                <NavLink href="/login">Sign in</NavLink>
+              </div>
+            </div>
+            {/* <div className="hidden md:flex md:gap-x-6">
+              <NavLink href="#features">Features</NavLink>
+              <NavLink href="#testimonials">Testimonials</NavLink>
+              <NavLink href="#pricing">Pricing</NavLink>
+            </div> */}
           </div>
         </nav>
       </Container>
     </header>
-  )
+  );
 }
